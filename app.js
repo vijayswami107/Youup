61e8090f1ddbd5d5fc4216c30042b4b39395b336
vijayswami107 <58056411+vijayswami107@users.noreply.github.com>
2024-09-05 22:59:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,139 @@
+var bodyParser  = require('body-parser'),
+    request     = require("request"),
+    express     = require("express"),
+    util        = require("util"),
+    fs          = require('fs'),
+    multer       = require("multer"),
+    GridFsStorage = require("multer-gridfs-storage"),
+    passport    = require("passport"),
+    popup       = require("sweetalert"),
+    passportLocal = require("passport-local"),
+    app         = express(),
+    mongoose    = require("mongoose"),
+    seed        = require("./seedDB"),
+    Tags        = require("./module/tag"),
+    Posts       = require("./module/post"),
+    Users       = require("./module/user");
+   
+
+var authRouts       = require('./routs/authRouts'),
+    postRouts       = require('./routs/postRouts'),
+    userRouts       = require('./routs/userRouts'),
+    otherRouts      = require('./routs/otherRouts'),
+    tagRouts        = require('./routs/tagRouts');
+
+passport.use(Users.createStrategy());
+
+//connecting database
+// mongoose.connect("mongodb://localhost/YouUp_6",{useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect("mongodb+srv://aditya:iamtheaditya@youup.zew8k.mongodb.net/YouUp?retryWrites=true&w=majority",{useNewUrlParser:true, useUnifiedTopology:true});
+
+//defining directories as public to use them in web 
+app.use(express.static("public"));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/views/home_page'));
+app.use(express.static(__dirname + '/views/template'));
+app.use(express.static(__dirname + '/views/partials'));
+app.use(express.static(__dirname + '/views/other'));
+app.use(express.static(__dirname + '/views/css'));
+app.use(express.static(__dirname + '/views/tag_landing'));
+app.use(express.static(__dirname + '/views/post_template'));
+app.use(express.static(__dirname + '/views/errors'));
+app.use(express.static(__dirname + './module'));
+app.use(express.static(__dirname + '/views/profile_template'));
+app.use(express.static(__dirname + '/views/authentication'));
+
+//passport configuration using passport package 
+app.use(require("express-session")({
+    secret: "You can be up with up",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.json());
+
+passport.use(new passportLocal(Users.authenticate()));
+
+passport.serializeUser(Users.serializeUser());
+passport.deserializeUser(Users.deserializeUser());
+
+
+//setting view engin as a ejs formate file 
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended : true}));
+
+//making a middleware to pass current loged in user info
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next();
+}); 
+
+//function for checking a user is loged in or not
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/user/login");
+};
+
+
+//calling seedDB.js file which contain initial data of the web
+// seed();
+
+//defining function to use in routs
+function set_time() {
+
+    var div ;
+    var d = new Date();
+    var c_hour = d.getHours();
+    if(c_hour==12)
+    {
+        div = "PM";
+    }
+    else if(c_hour==24)
+    {
+        c_hour = 12;
+        div = "AM";
+    }
+    else if(c_hour>12 && c_hour<24)
+    {
+        c_hour = c_hour-12 ;
+        div = "PM";
+    }
+    else
+    {
+        div = "AM";
+    }
+    var c_min = d.getMinutes();
+    var c_sec = d.getSeconds();
+    var t = c_hour + ":" + c_min + ":" + c_sec + " "+div;
+    return t;
+}
+
+function set_date() {
+    var d = new Date();
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var date = d. getDate();
+    var month = d. getMonth() ; // Since getMonth() returns month from 0-11 not 1-12.
+    var year = d. getFullYear();
+    var dateStr = months[month] + " " + date  + ", " + year;
+    return dateStr;
+}
+
+
+//using custom routs 
+app.use(authRouts);
+app.use(tagRouts);
+app.use(postRouts);
+app.use(userRouts);
+app.use(otherRouts);
+
+//setting env for running application
+// app.listen(process.env.PORT ,process.env.IP, function () {
+//     console.log("App listening on port 3000!");
+// });
+app.listen(process.env.PORT ,process.env.IP, function () {
+    console.log("App listening on port 3000!");
+});
---
+++ seedDB.js
@@ -0,0 +1,239 @@
+var mongoose = require('mongoose'),
+    Tags = require("./module/tag"),
+    Posts = require("./module/post"),
+    Users = require("./module/user"),
+    Comments = require("./module/comment");
+
+var tags_data =
+[
+    {
+        name:"Tech",
+        image:"https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        name:"Education",
+        image:"https://images.unsplash.com/photo-1546410531-bb4caa6b424d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    
+    },
+    {
+        name:"Art",
+        image:"https://images.unsplash.com/photo-1526304760382-3591d3840148?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        
+    },
+    {
+        name:"Sports",
+        image:"https://images.unsplash.com/photo-1541983663620-7571a820610c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    
+    },
+    {
+        name:"Food",
+        image:"https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        
+    },
+    {
+        name:"Science",
+        image:"https://images.unsplash.com/photo-1567427018141-0584cfcbf1b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        
+    },
+    {
+        name:"Business",
+        image:"https://images.unsplash.com/photo-1444653614773-995cb1ef9efa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+    },
+    {
+        name : "Astrology",
+        image :"https://images.unsplash.com/photo-1503751071777-d2918b21bbd9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        name : "Travel",
+        image: "https://images.unsplash.com/photo-1503220317375-aaad61436b1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+    }
+   
+]
+
+var post_data = [
+    {
+        name        : "first post_1",
+        image       : "https://images.unsplash.com/photo-1592080316965-31d9d2f45762?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"
+    }
+    // {
+    //     name        : "first post_2",
+    //     image       : "https://images.unsplash.com/photo-1592079927431-3f8ced0dacc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    //     description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"
+    // }
+    // {
+    //     name        : "four post_1",
+    //     image       : "https://images.unsplash.com/photo-1532040683343-edbde6dd500d?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=525&q=60",
+    //     description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"
+    // },
+    // {
+    //     name          : "four post_2",
+    //     image       : "https://images.unsplash.com/photo-1568918460973-fe7f54f82482?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=525&q=60",
+    //     description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"
+    // },
+    // {
+    //     name        : "third post_1",
+    //     image       : "https://images.unsplash.com/photo-1580339475952-d3ced325ad90?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=525&q=60",
+    //     description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"
+    // },
+    // {
+    //     name        : "third post_2",
+    //     image       : "https://images.unsplash.com/photo-1552609034-ae1a64c72b5e?ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=525&q=60",
+    //     description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"
+    // }
+
+]
+
+
+var users_data = [
+    {
+        user_name   : "a_234",
+        name        : "Adityasdf",
+        image       : "https://images.unsplash.com/photo-1592079927431-3f8ced0dacc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        place       : "New York",
+        field       : "Technical",
+    },
+    {
+        user_name   : "a_23432",
+        name        : "Adityabgh",
+        image       : "https://images.unsplash.com/photo-1592079927431-3f8ced0dacc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        place       : "New York",
+        field       : "Technical",
+    },
+    {
+        user_name   : "a_2345we",
+        name        : "Adityawerte",
+        image       : "https://images.unsplash.com/photo-1592079927431-3f8ced0dacc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        place       : "New York",
+        field       : "Technical",
+    },
+    {
+        user_name   : "a_2342345",
+        name        : "Adityavnb",
+        image       : "https://images.unsplash.com/photo-1592079927431-3f8ced0dacc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        place       : "New York",
+        field       : "Technical",
+    }
+]
+
+//creating new tag with the help of our database and in each tag creating new post using post_data
+function seedDB(){
+
+    Tags.remove({},function(err){
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("Tag removed");
+        }
+    });
+
+    Users.remove({},function(err){
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("User removed");
+        }
+    });
+
+    Posts.remove({},function(err){
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("Post removed");
+            
+        }
+    });
+
+    Comments.remove({},function(err){
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("Comments removed");
+        }
+    });
+    
+
+    tags_data.forEach(function(seed){
+        Tags.create(seed,function(err,tag){
+            if(err)
+            {
+                console.log(err);
+            }
+            else
+            {
+                console.log("New tag added..!");
+                // post_data.forEach(function(post_seed){
+                //     Posts.create(post_seed,function(err,post_data){
+                //         if(err)
+                //         {
+                //             console.log(err);
+                //         }
+                //         else
+                //         {
+                //             console.log("New Post added..!");
+                //             tag.post.push(post_data);
+                //             tag.save();
+                //             // tag.save(function(err,saved_data){
+                //             //     if(err)
+                //             //     {
+                //             //         console.log(err);
+                //             //     }
+                //             //     else
+                //             //     {
+                //             //         console.log("work Done..!");
+                //             //     }
+                //             // });
+                //         }
+                //     });
+                // });
+            }
+        });
+    });
+
+    // // users_data.forEach(function(seed){
+    // //     Users_detail.create(seed,function(err,user_detail){
+    // //         if(err)
+    // //         {
+    // //             console.log(err);
+    // //         }
+    // //         else
+    // //         {
+    // //             console.log("New user added..!");
+    // //             post_data.forEach(function(post_seed){
+    // //                 Posts.create(post_seed,function(err,post_data){
+    // //                     if(err)
+    // //                     {
+    // //                         console.log(err);
+    // //                     }
+    // //                     else
+    // //                     {
+    // //                         console.log("New Post added..!");
+    // //                         user_detail.post.push(post_data);
+    // //                         user_detail.save();
+                            
+    // //                     }
+    // //                 });
+    // //             });
+    // //         }
+    // //     });
+    // // });
+};
+
+
+
+//exporting seedDB function use this in another file 
+
+module.exports = seedDB;
